0f72082d2a4b78b624d0a160d732532775a4fc2b
Lucio <luciobecklerp@gmail.com>
2022-09-26 19:30:08 -0300

    Projeto concluído

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-let numbers = document.querySelectorAll(".circle");
-let button = document.querySelectorAll(".submit");
-let mainBox = document
+let ratings = document.querySelectorAll(".circle");
+let button = document.querySelector(".submit");
+let mainBox = document.querySelector(".mainBox");
+let number = document.querySelector(".number");
+let thanksBox = document.querySelector(".thanksBox");
+
+
+for (let i = 0; i < ratings.length; i++) {
+      ratings[i].addEventListener('click', function () {  /* Escuta os circles a procura de um clique e aplica a função ao lado quando ocorre */
+            let rattingScore = ratings[i].innerHTML;     /* Captura o valor correspondente ao botão clicado */
+
+            for (let i = 0; i < ratings.length; i++) {         /*Sempre que ocorre um novo clique este for realoca a classe 'selectedCircle' */
+                  ratings[i].classList.remove('selectedCircle')
+            }
+            ratings[i].classList.add('selectedCircle');
+
+            button.addEventListener('click', function () {  /* Escuta o botão submit e aplica a função ao lado quando houver um clique */
+                  mainBox.style.display = 'none';     /* Altera o parâmetro display dos elementos mainBox e thanksBox */
+                  thanksBox.style.display = 'flex';
+                  number.innerHTML = rattingScore;
+            });
+
+            thanksBox.addEventListener('click', function () {
+                  mainBox.style.display = 'flex';
+                  thanksBox.style.display = 'none';
+            })
+      })
+}
